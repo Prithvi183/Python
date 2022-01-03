0ef43a560086f9c2a5b34b6a6dd7eeb697016f32
Prithviraj <prithvi.patil183@gmail.com>
2022-01-03 10:52:38 +0530

    Added Firstprogram file

---
+++ Firstprogram.py
@@ -0,0 +1,23 @@
+#Basic code
+print("Hello, Coders!!")
+
+#Varaibles
+name = "Prithviraj"
+age = 19
+is_adult = True
+
+print(name)
+print(age)
+print(is_adult)
+
+#input function
+name = input("What is your name?: ")
+
+print("Hello " + name)
+
+#type conversions
+old_age = input("Enter your old age: ")
+new_age = int(old_age) + 2
+
+print("Your current age is "+new_age)
+
