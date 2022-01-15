35b29fcf219225604890f4f14f6a1041fdecc4b7
Prithviraj <prithvi.patil183@gmail.com>
2022-01-15 19:52:38 +0530

    Updated File

--- Firstprogram.py
+++ Firstprogram.py
@@ -1,23 +1,38 @@
-#Basic code
-print("Hello, Coders!!")
+# #Basic code
+# print("Hello, Coders!!")
 
-#Varaibles
-name = "Prithviraj"
-age = 19
-is_adult = True
+# #Varaibles
+# name = "Prithviraj"
+# age = 19
+# is_adult = True
 
-print(name)
-print(age)
-print(is_adult)
+# print(name)
+# print(age)
+# print(is_adult)
 
-#input function
-name = input("What is your name?: ")
+# #input function
+# name = input("What is your name?: ")
+# print("Hello " + name)
 
-print("Hello " + name)
+# #type conversions
+# old_age = input("Enter your old age: ")
+# new_age = int(old_age) + 2
+# print("Your current age is "+new_age)
 
-#type conversions
-old_age = input("Enter your old age: ")
-new_age = int(old_age) + 2
+# #Sum of two numbers
+# num1 = input("Enter your first number: ")
+# num2 = input("Enter your second number: ")
+# sum = int(num1) + int(num2)
+# print("The sum is "+ str(sum))
 
-print("Your current age is "+new_age)
+# #strings
+# name = "Prithviraj"
+# print(name.upper())
+# print(name.lower())
+# print(name.find('i'))
+# print(name.replace("Prithviraj", "Rahul"))
 
+#Finding char/str in string
+name1 = "Prithviraj"
+print("raj" in name1)
+print("rahul" in name1)
